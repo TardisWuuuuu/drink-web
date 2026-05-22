417442fc382687d313973587e90ccc75200e2013
TardisWuuuuu <tardis22@qq.com>
2026-05-22 12:22:18 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -4,22 +4,35 @@ const path = require('path');
 const app = express();
 
 app.use(express.json());
-app.use(express.static('./'));
+app.use(express.static(__dirname));
 
-const dbPath = path.join(__dirname, 'db.json');
-if (!fs.existsSync(dbPath)) fs.writeFileSync(dbPath, JSON.stringify([]));
+const dataFile = path.join(__dirname, 'data.json');
 
-app.get('/getList', (req, res) => {
-  res.json(JSON.parse(fs.readFileSync(dbPath)));
+// 自动创建数据文件
+if (!fs.existsSync(dataFile)) {
+  fs.writeFileSync(dataFile, JSON.stringify({ list: [] }));
+}
+
+// 获取数据
+app.get('/api/get', (req, res) => {
+  try {
+    const data = JSON.parse(fs.readFileSync(dataFile, 'utf8'));
+    res.json(data);
+  } catch (e) {
+    res.json({ list: [] });
+  }
 });
 
-app.post('/saveList', (req, res) => {
-  fs.writeFileSync(dbPath, JSON.stringify(req.body, null, 2));
-  res.send('ok');
+// 保存数据
+app.post('/api/save', (req, res) => {
+  fs.writeFileSync(dataFile, JSON.stringify(req.body, null, 2));
+  res.json({ ok: true });
 });
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 app.get('/submit.html', (req, res) => res.sendFile(path.join(__dirname, 'submit.html')));
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log('Running'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log('服务已启动');
+});
