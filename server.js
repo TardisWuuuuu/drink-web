98899992d6494da48aeb38fc7d72c2343e32ccd5
TardisWuuuuu <tardis22@qq.com>
2026-05-22 09:41:58 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -2,20 +2,13 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-// 静态文件自动托管
 app.use(express.static(__dirname));
+app.use(express.json());
 
-// 首页
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// 投稿页
-app.get('/submit.html', (req, res) => {
-  res.sendFile(path.join(__dirname, 'submit.html'));
-});
-
-// 关键：Railway 必须这样写才不会 502
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
   console.log("✅ 服务启动成功");
